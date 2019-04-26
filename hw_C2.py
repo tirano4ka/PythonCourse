8bf2631d7612bae900940f484d18bf02471c7a9b
tirano4ka <48602892+tirano4ka@users.noreply.github.com>
2019-04-26 13:57:44 +0300

    Update hw_C2.py

--- hw_C2.py
+++ hw_C2.py
@@ -7,13 +7,4 @@ for i in n:
     mult *= int(i)
  
 print("Сумма цифр:", summa)
-print("Произведение значащих цифр:", mult)n = input()
-mult = 1
-summa = 0
-for i in n:
-  summa += int(i)
-  if int(i) != 0:
-    mult *= int(i)
- 
-print("Сумма цифр:", summa)
 print("Произведение значащих цифр:", mult)
